7f32dc91cfb3d3996f97f369f34fefaa578ec1f4
李会祥 <1437088404@qq.com>
2019-08-06 21:47:41 +0800

    init

---
+++ PayDemo.Subprocess/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PayDemo.Subprocess
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            return Chromely.CefGlue.Subprocess.Subprocess.Execute(args);
+        }
+    }
+}
---
+++ PayDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Chromely.CefGlue;
+using Chromely.Core;
+using Chromely.Core.Helpers;
+using Chromely.Core.Host;
+
+namespace PayDemo
+{
+    public static class Program
+    {
+        public static IChromelyWindow MainWindow { get; private set; }
+        public static int Main(string[] args)
+        {
+            var appDir = AppDomain.CurrentDomain.BaseDirectory;
+
+            const string startUrl = "local://app/index.html";
+
+            var config = ChromelyConfiguration
+                .Create()
+                .WithAppArgs(args)
+                .WithStartUrl(startUrl)
+                ;
+
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Win32S:
+                case PlatformID.Win32Windows:
+                case PlatformID.Win32NT:
+                case PlatformID.WinCE:
+                case PlatformID.Xbox:
+                    config.UseDefaultSubprocess();
+                    break;
+                case PlatformID.Unix:
+                    config.UseDefaultSubprocess(false)
+                        .WithCustomSetting(CefSettingKeys.BrowserSubprocessPath, appDir + "PayDemo.Subprocess")
+                        ;
+                    break;
+                case PlatformID.MacOSX:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            using (var window=ChromelyWindow.Create(config))
+            {
+                MainWindow = window;
+                MainWindow.ScanAssemblies();
+                return MainWindow.Run(args);
+            }
+        }
+    }
+}
